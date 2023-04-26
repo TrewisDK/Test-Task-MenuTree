e7ea050e25ecee8ff5c60c5bd194d174ed27c096
TrewisDK <76664627+TrewisDK@users.noreply.github.com>
2023-04-26 20:59:51 +0300

    Menu ready, working through the hierarchy

--- menu_tree_up/models.py
+++ menu_tree_up/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 
 class Menu(models.Model):
+    """ Модель меню, нужно для того что-бы вызывать меню по name в соответсвии с ТЗ """
     name = models.CharField("Название меню", max_length=30)
 
     def __str__(self):
@@ -10,16 +11,21 @@ class Menu(models.Model):
 
 
 class MenuPoint(models.Model):
+    """ Модель пунктов меню """
+    # Название пункта меню
     name = models.CharField("Назавание пункта", max_length=30)
+    # Отношение к модели меню для плучения пунктов при вызове меню
     menu = models.ForeignKey(Menu, related_name='current_menu_points', on_delete=models.CASCADE, null=True)
+    # URL адресс меню
     url = models.CharField("URL", max_length=100)
+    # Родительский элемент пункта меню, ссылается сам на имеет related_name для получения дочерних элеменов пункта меню
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-    def check_absolute_url(self):
-        if self.url:
+    def get_absolute_url(self):
+        if self.url.startswith("http"):
             return self.url
         else:
-            reverse(self.url)
+            return reverse(f"{self.url}")
--- menu_tree_up/templatetags/draw_menu.py
+++ menu_tree_up/templatetags/draw_menu.py
@@ -4,8 +4,12 @@ from menu_tree_up.models import Menu
 register = template.Library()
 
 
-@register.inclusion_tag("custom_tags/menu_tags/menu_template.html")
-def draw_menu(menu_name):
-    menu_points = Menu.objects.get(name=menu_name).current_menu_points.all
-    return {"menu_points" : menu_points}
+@register.inclusion_tag("custom_tags/menu_tags/menu_template.html", takes_context=True)
+def draw_menu(context, menu_name):
+    """ Функция отвечающая за отображение меню на странице
+    Принимает аргументы context - для получения из request текущего url адреса, а так же menu-name для получения меню,
+    которое нужно отобразить """
 
+    # По 1 запросу получаем меню, а затем получаем из него его пункты
+    menu_points = Menu.objects.get(name=menu_name).current_menu_points.all
+    return {"menu_points": menu_points, 'request': context['request']}
